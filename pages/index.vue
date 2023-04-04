165f9fdec2cf329e9179fa91785478938b5be12a
chenchao <chenchaowd@gmail.com>
2023-04-04 10:01:55 +0800

    feat: outlink

--- pages/index.vue
+++ pages/index.vue
@@ -12,28 +12,24 @@
                         </div>
                     </div>
                     <div class="flex items-center justify-evenly h-10 ml-30">
-                        <n-button strong secondary class="m-2 flex-1">
-                            <template #icon>
-                                <n-icon><logo-github /></n-icon>
+                        <n-button strong v-bind="{ ...item.buttonAttr }" :class="`outlink-${item.icon}`"
+                            class="outlink-button-icon m-2 flex-1" v-for="(item, index) in outlinkList" :key="index">
+                            <template #icon v-if="item.icon">
+                                <!-- <n-icon :component="item.icon" v-bind="{ ...item.iconAttr }" /> -->
+                                <!-- <img :src="requireImg('img/' + item.icon)" v-bind="{ ...item.iconAttr }" alt=""> -->
+                                <div class="icon"></div>
                             </template>
-                            github
-                        </n-button>
-                        <n-button strong secondary class="m-2 flex-1">
-                            <template #icon>
-                                <n-icon><logo-github /></n-icon>
-                            </template>
-                            github
+                            <span v-if="item.name"> github </span>
                         </n-button>
                     </div>
                 </div>
-
                 <div>
                     <div>
                         <h1 class="pada-title">Projects</h1>
                     </div>
                     <div class="flex">
-                        <n-card class="m-3  flex-1" :title="item.packageName" hoverable v-for="item in ProjectsList"
-                            :key="item.id">
+                        <n-card class="mr-3 flex-1 cursor-pointer" :title="item.packageName" hoverable
+                            v-for="item in ProjectsList" :key="item.id">
                             <div class="flex items-center">
                                 <img class="w-10 mr-5" :src="requireImg('img/' + item.icon)" alt="">
                                 <span>1</span>
@@ -50,7 +46,7 @@
     </NuxtLayout>
 </template>
 <script lang="ts" setup>
-import { LogoGithub } from "@vicons/ionicons5"
+// import { LogoGithub } from "@vicons/ionicons5"
 
 const ProjectsList = [
     {
@@ -72,11 +68,65 @@ const ProjectsList = [
         githubUrl: 'https://www.naiveui.com/zh-CN/os-theme/components/icon'
     }
 ]
+
+const outlinkList = [
+    {
+        icon: 'github',
+        iconAttr: {},
+        buttonAttr: {},
+        name: 'github',
+        url: 'https://www.naiveui.com/zh-CN/os-theme/components/icon'
+    },
+    {
+        icon: 'bilibili',
+        iconAttr: {},
+        url: 'https://www.naiveui.com/zh-CN/os-theme/components/icon'
+    }
+]
 </script>
 
 
 <style scoped lang="less">
 .pada-title {
-    @apply text-2xl m-5;
+    @apply text-2xl mt-5 mb-5;
 }
-</style>
+
+.outlink-button-icon { 
+    .icon {
+        @apply transition-all duration-300 h-full w-full
+    }
+    &:hover {
+        .icon {
+            @apply transition-all duration-300
+        }
+    }
+}
+
+
+// .outlink-button-icon {
+//     .icon {
+//         height: 100%;
+//         width: 100%;
+//         transition: all @transition-speed;
+//     }
+
+//     &:hover {
+//         .icon {
+//             transition: all @transition-speed;
+//         }
+//     }
+// }
+
+.outlink-github {
+    .icon {
+        background: url('~/assets/img/github.svg') no-repeat center;
+    }
+
+    &:hover {
+        .icon {
+            background: url('~/assets/img/douyin.svg') no-repeat center;
+        }
+
+        background-color: aqua;
+    }
+}</style>
